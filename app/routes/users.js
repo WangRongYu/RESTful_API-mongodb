fc234384281dcf889f0076c393264604c427d7bc
wangrongyu <19802021027@139.com>
2019-09-11 09:25:16 +0800

    将自己编写的鉴权中间件更改为koa-jwt

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,4 +1,5 @@
-const jsonwebtoken = require('jsonwebtoken');
+// const jsonwebtoken = require('jsonwebtoken');
+const jwt = require('koa-jwt');
 const Router = require('koa-router');
 const router = new Router({prefix: '/users'});
 const { find, findById, create, 
@@ -6,17 +7,7 @@ const { find, findById, create,
 } = require('../controllers/users');
 
 const { secret } = require('../config');
-const auth = async (ctx,next) => {
-    const { authorization = '' } = ctx.request.header;
-    const token = authorization.replace('Bearer ', '');
-    try{
-        const user = jsonwebtoken.verify(token,secret);
-        ctx.state.user = user;
-    }catch(err){
-        ctx.throw(401,err.message);
-    }
-    await next();
-}
+const auth = jwt( { secret } );
 
 router.get('/',find);
 
